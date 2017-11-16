a6f7f22932d72677c7515d2976a1e487a0b9ffe5
一杯冰啤 <cy@ymify.com>
2017-11-16 14:54:36 +0800

    新增功能

--- js/cart.js
+++ js/cart.js
@@ -3,7 +3,10 @@ new Vue({
 	data: {
 		title: 'hello vue',
 		products:[],
-		checkAllFlag: true,
+		checkAllFlag: false,
+		totalPrice: 0,
+		delFlag: false,
+		targetProduct: '',
 	},
 	filters: {
 		formatMoney:function(value){
@@ -32,7 +35,8 @@ new Vue({
 				if (item.count>1) {
 					item.count--;
 				};
-			}
+			};
+			this.calcTotalPrice();
 		},
 		selectProduct: function(item) {
 			// 知识点，如何利用vue设置一个不存在的变量（即json里没有声明的变量）
@@ -40,12 +44,65 @@ new Vue({
 				// Vue.set(item,'checked',true);
 				//局部注册checked方法
 				this.$set(item,'checked',true);
+				// console.log(item.checked);
 			} else {
 				item.checked = !item.checked;
-			}
+			};
+
+			// 一个个选中后全选，方法一：
+			// var k = 0;
+			// this.products.forEach(function(item,index){
+			// 	if (item.checked) {k++;} 
+			// });
+			// if (k == this.products.length) {
+			// 	this.checkAllFlag = true;
+			// } else {
+			// 	this.checkAllFlag = false;
+			// }
+
+			// 一个个选中后全选，方法二：
+			var isAll = this.products.every((item, index) => {
+				return item.checked === true;
+				console.log(this);
+			});
+			this.checkAllFlag = isAll ? true : false;
+
+			this.calcTotalPrice();
 		},
 		checkAll:function(){
+			this.checkAllFlag = !this.checkAllFlag;//全选按钮添加/删除check类名
 			
+			var _this = this;
+			this.products.forEach(function(item,index){
+				if (item.checked == undefined) { //一上来直接点全选的时候，上面物品还没有注册checked
+					_this.$set(item,'checked',true);
+				} else {
+					item.checked = _this.checkAllFlag;
+				}
+			});
+			this.calcTotalPrice();
+		},
+		// 计算总金额
+		calcTotalPrice: function(){
+			var _this = this;
+			this.totalPrice = 0;
+			this.products.forEach(function(item,index){
+				if (item.checked) {
+					_this.totalPrice += item.price*item.count;
+				};
+			});
+		},
+		delConfirm: function(item){
+			this.delFlag = true;
+			this.targetProduct = item;
+			console.log(this.targetProduct);//获取到了想要删除的这个对象
+		},
+		delProduct: function() {
+			var index = this.products.indexOf(this.targetProduct);
+			console.log(index);
+			this.products.splice(index,1);
+			this.delFlag = false;
+			this.calcTotalPrice();
 		}
 	}
 });
